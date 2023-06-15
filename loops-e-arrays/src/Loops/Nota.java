45c8283a1976e69cfbbe82b6c519ebfa101d26d6
Fabio H Oliveira <fabio_oliveira231@hotmail.com>
2023-06-14 21:53:07 -0300

    Resolução de exercicios

---
+++ loops-e-arrays/src/Loops/Nota.java
@@ -0,0 +1,22 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Nota {
+    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int nota;
+
+        System.out.println("Digite a nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 | nota > 10){
+            System.out.println("A nota é inválida. Digite novamente: ");
+            nota = scan.nextInt();
+        }
+
+    }
+}
